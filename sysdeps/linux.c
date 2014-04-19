872a48db6508372b57f15ce7adfde0682bf6aec8
Igor Pashev <pashev.igor@gmail.com>
2014-04-19 21:30:39 +0400

    Formatted with GNU indent -nut

--- sysdeps/linux.c
+++ sysdeps/linux.c
@@ -1,3 +1,5 @@
+/* Formatted with GNU indent -nut */
+
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
 #endif
@@ -17,71 +19,81 @@
 #include "htop-sysdeps.h"
 #include "utils.h"
 
-IOPriority sysdep_get_ioprio(pid_t p)
+IOPriority
+sysdep_get_ioprio (pid_t p)
 {
-    IOPriority ioprio = syscall(SYS_ioprio_get, IOPRIO_WHO_PROCESS, p);
-    return ioprio;
+  IOPriority ioprio = syscall (SYS_ioprio_get, IOPRIO_WHO_PROCESS, p);
+  return ioprio;
 }
 
-void sysdep_set_ioprio(pid_t p, IOPriority ioprio)
+void
+sysdep_set_ioprio (pid_t p, IOPriority ioprio)
 {
-    syscall(SYS_ioprio_get, IOPRIO_WHO_PROCESS, p);
+  syscall (SYS_ioprio_get, IOPRIO_WHO_PROCESS, p);
 }
 
-void sysdep_get_meminfo(ProcessList *this)
+void
+sysdep_get_meminfo (ProcessList * this)
 {
-   unsigned long long int swapFree = 0;
+  unsigned long long int swapFree = 0;
 
-   FILE* file = fopen(PROCMEMINFOFILE, "r");
-   if (file == NULL) {
-      CRT_fatalError("Cannot open " PROCMEMINFOFILE);
-   }
-   {
-      char buffer[128];
-      while (fgets(buffer, 128, file)) {
+  FILE *file = fopen (PROCMEMINFOFILE, "r");
+  if (file == NULL)
+    {
+      CRT_fatalError ("Cannot open " PROCMEMINFOFILE);
+    }
+  {
+    char buffer[128];
+    while (fgets (buffer, 128, file))
+      {
 
-         switch (buffer[0]) {
-         case 'M':
-            if (String_startsWith(buffer, "MemTotal:"))
-               sscanf(buffer, "MemTotal: %llu kB", &this->totalMem);
-            else if (String_startsWith(buffer, "MemFree:"))
-               sscanf(buffer, "MemFree: %llu kB", &this->freeMem);
-            else if (String_startsWith(buffer, "MemShared:"))
-               sscanf(buffer, "MemShared: %llu kB", &this->sharedMem);
+        switch (buffer[0])
+          {
+          case 'M':
+            if (String_startsWith (buffer, "MemTotal:"))
+              sscanf (buffer, "MemTotal: %llu kB", &this->totalMem);
+            else if (String_startsWith (buffer, "MemFree:"))
+              sscanf (buffer, "MemFree: %llu kB", &this->freeMem);
+            else if (String_startsWith (buffer, "MemShared:"))
+              sscanf (buffer, "MemShared: %llu kB", &this->sharedMem);
             break;
-         case 'B':
-            if (String_startsWith(buffer, "Buffers:"))
-               sscanf(buffer, "Buffers: %llu kB", &this->buffersMem);
+          case 'B':
+            if (String_startsWith (buffer, "Buffers:"))
+              sscanf (buffer, "Buffers: %llu kB", &this->buffersMem);
             break;
-         case 'C':
-            if (String_startsWith(buffer, "Cached:"))
-               sscanf(buffer, "Cached: %llu kB", &this->cachedMem);
+          case 'C':
+            if (String_startsWith (buffer, "Cached:"))
+              sscanf (buffer, "Cached: %llu kB", &this->cachedMem);
             break;
-         case 'S':
-            if (String_startsWith(buffer, "SwapTotal:"))
-               sscanf(buffer, "SwapTotal: %llu kB", &this->totalSwap);
-            if (String_startsWith(buffer, "SwapFree:"))
-               sscanf(buffer, "SwapFree: %llu kB", &swapFree);
+          case 'S':
+            if (String_startsWith (buffer, "SwapTotal:"))
+              sscanf (buffer, "SwapTotal: %llu kB", &this->totalSwap);
+            if (String_startsWith (buffer, "SwapFree:"))
+              sscanf (buffer, "SwapFree: %llu kB", &swapFree);
             break;
-         }
+          }
       }
-   }
+  }
 
-   this->usedMem = this->totalMem - this->freeMem;
-   this->usedSwap = this->totalSwap - swapFree;
-   fclose(file);
+  this->usedMem = this->totalMem - this->freeMem;
+  this->usedSwap = this->totalSwap - swapFree;
+  fclose (file);
 }
 
 
-void sysdep_update_cpu_data(ProcessList *this)
+void
+sysdep_update_cpu_data (ProcessList * this)
 {
-   unsigned long long int usertime, nicetime, systemtime, systemalltime, idlealltime, idletime, totaltime, virtalltime;
+  unsigned long long int usertime, nicetime, systemtime, systemalltime,
+    idlealltime, idletime, totaltime, virtalltime;
 
-   FILE *file = fopen(PROCSTATFILE, "r");
-   if (file == NULL) {
-      CRT_fatalError("Cannot open " PROCSTATFILE);
-   }
-   for (int i = 0; i <= this->cpuCount; i++) {
+  FILE *file = fopen (PROCSTATFILE, "r");
+  if (file == NULL)
+    {
+      CRT_fatalError ("Cannot open " PROCSTATFILE);
+    }
+  for (int i = 0; i <= this->cpuCount; i++)
+    {
       char buffer[256];
       int cpuid;
       unsigned long long int ioWait, irq, softIrq, steal, guest;
@@ -89,22 +101,27 @@ void sysdep_update_cpu_data(ProcessList *this)
       // Dependending on your kernel version,
       // 5, 7 or 8 of these fields will be set.
       // The rest will remain at zero.
-      fgets(buffer, 255, file);
+      fgets (buffer, 255, file);
       if (i == 0)
-         sscanf(buffer, "cpu  %llu %llu %llu %llu %llu %llu %llu %llu %llu",
-                 &usertime, &nicetime, &systemtime, &idletime, &ioWait, &irq, &softIrq, &steal, &guest);
-      else {
-         sscanf(buffer, "cpu%d %llu %llu %llu %llu %llu %llu %llu %llu %llu",
-         &cpuid, &usertime, &nicetime, &systemtime, &idletime, &ioWait, &irq, &softIrq, &steal, &guest);
-         assert(cpuid == i - 1);
-      }
+        sscanf (buffer, "cpu  %llu %llu %llu %llu %llu %llu %llu %llu %llu",
+                &usertime, &nicetime, &systemtime, &idletime, &ioWait, &irq,
+                &softIrq, &steal, &guest);
+      else
+        {
+          sscanf (buffer,
+                  "cpu%d %llu %llu %llu %llu %llu %llu %llu %llu %llu",
+                  &cpuid, &usertime, &nicetime, &systemtime, &idletime,
+                  &ioWait, &irq, &softIrq, &steal, &guest);
+          assert (cpuid == i - 1);
+        }
       // Fields existing on kernels >= 2.6
       // (and RHEL's patched kernel 2.4...)
       idlealltime = idletime + ioWait;
       systemalltime = systemtime + irq + softIrq;
       virtalltime = steal + guest;
-      totaltime = usertime + nicetime + systemalltime + idlealltime + virtalltime;
-      CPUData* cpuData = &(this->cpus[i]);
+      totaltime =
+        usertime + nicetime + systemalltime + idlealltime + virtalltime;
+      CPUData *cpuData = &(this->cpus[i]);
       assert (usertime >= cpuData->userTime);
       assert (nicetime >= cpuData->niceTime);
       assert (systemtime >= cpuData->systemTime);
@@ -141,116 +158,128 @@ void sysdep_update_cpu_data(ProcessList *this)
       cpuData->stealTime = steal;
       cpuData->guestTime = guest;
       cpuData->totalTime = totaltime;
-   }
-   fclose(file);
+    }
+  fclose (file);
 }
 
-int sysdep_max_pid()
+int
+sysdep_max_pid ()
 {
-   int maxPid = 4194303;
-   FILE* file = fopen("/proc/sys/kernel/pid_max", "r");
-   if (file) {
-       fscanf(file, "%32d", &maxPid);
-       fclose(file);
-   }
-   return maxPid;
+  int maxPid = 4194303;
+  FILE *file = fopen ("/proc/sys/kernel/pid_max", "r");
+  if (file)
+    {
+      fscanf (file, "%32d", &maxPid);
+      fclose (file);
+    }
+  return maxPid;
 }
 
 
-bool sysdep_get_process_info(Process *process, pid_t pid)
+bool
+sysdep_get_process_info (Process * process, pid_t pid)
 {
-   char filename[MAX_NAME+1];
-   static char buf[MAX_READ+1];
-   int fd;
-   ssize_t size;
+  char filename[MAX_NAME + 1];
+  static char buf[MAX_READ + 1];
+  int fd;
+  ssize_t size;
 
-   snprintf(filename, MAX_NAME, "/proc/%d/stat", pid);
-   fd = open(filename, O_RDONLY);
-   if (fd == -1) return false;
+  snprintf (filename, MAX_NAME, "/proc/%d/stat", pid);
+  fd = open (filename, O_RDONLY);
+  if (fd == -1)
+    return false;
 
-   size = xread(fd, buf, MAX_READ);
-   close(fd);
-   if (!size) return false;
-   buf[size] = '\0';
+  size = xread (fd, buf, MAX_READ);
+  close (fd);
+  if (!size)
+    return false;
+  buf[size] = '\0';
 
-   assert(process->pid == atoi(buf));
-   char *location = strchr(buf, ' ');
-   if (!location) return false;
+  assert (process->pid == atoi (buf));
+  char *location = strchr (buf, ' ');
+  if (!location)
+    return false;
 
-   location += 2;
-   char *end = strrchr(location, ')');
-   if (!end) return false;
+  location += 2;
+  char *end = strrchr (location, ')');
+  if (!end)
+    return false;
 
-   process->comm = strndup(location, end - location);
-   
-   location = end + 2;
+  process->comm = strndup (location, end - location);
 
-   process->state = location[0];
-   location += 2;
-   process->ppid = strtol(location, &location, 10);
-   location += 1;
-   process->pgrp = strtoul(location, &location, 10);
-   location += 1;
-   process->session = strtoul(location, &location, 10);
-   location += 1;
-   process->tty_nr = strtoul(location, &location, 10);
-   location += 1;
-   process->tpgid = strtol(location, &location, 10);
-   location += 1;
-   process->flags = strtoul(location, &location, 10);
-   location += 1;
-   location = strchr(location, ' ')+1;
-   location = strchr(location, ' ')+1;
-   location = strchr(location, ' ')+1;
-   location = strchr(location, ' ')+1;
-   process->utime = strtoull(location, &location, 10);
-   location += 1;
-   process->stime = strtoull(location, &location, 10);
-   location += 1;
-   process->cutime = strtoull(location, &location, 10);
-   location += 1;
-   process->cstime = strtoull(location, &location, 10);
-   location += 1;
-   process->priority = strtol(location, &location, 10);
-   location += 1;
-   process->nice = strtol(location, &location, 10);
-   location += 1;
-   process->nlwp = strtol(location, &location, 10);
-   location += 1;
-   for (int i=0; i<17; i++) location = strchr(location, ' ')+1;
-   process->exit_signal = strtol(location, &location, 10);
-   location += 1;
-   assert(location != NULL);
-   process->processor = strtol(location, &location, 10);
-   assert(location == NULL);
+  location = end + 2;
+
+  process->state = location[0];
+  location += 2;
+  process->ppid = strtol (location, &location, 10);
+  location += 1;
+  process->pgrp = strtoul (location, &location, 10);
+  location += 1;
+  process->session = strtoul (location, &location, 10);
+  location += 1;
+  process->tty_nr = strtoul (location, &location, 10);
+  location += 1;
+  process->tpgid = strtol (location, &location, 10);
+  location += 1;
+  process->flags = strtoul (location, &location, 10);
+  location += 1;
+  location = strchr (location, ' ') + 1;
+  location = strchr (location, ' ') + 1;
+  location = strchr (location, ' ') + 1;
+  location = strchr (location, ' ') + 1;
+  process->utime = strtoull (location, &location, 10);
+  location += 1;
+  process->stime = strtoull (location, &location, 10);
+  location += 1;
+  process->cutime = strtoull (location, &location, 10);
+  location += 1;
+  process->cstime = strtoull (location, &location, 10);
+  location += 1;
+  process->priority = strtol (location, &location, 10);
+  location += 1;
+  process->nice = strtol (location, &location, 10);
+  location += 1;
+  process->nlwp = strtol (location, &location, 10);
+  location += 1;
+  for (int i = 0; i < 17; i++)
+    location = strchr (location, ' ') + 1;
+  process->exit_signal = strtol (location, &location, 10);
+  location += 1;
+  assert (location != NULL);
+  process->processor = strtol (location, &location, 10);
+  assert (location == NULL);
 
 
-   /* Try to get full command line */
-   snprintf(filename, MAX_NAME, "/proc/%d/cmdline", pid);
-   fd = open(filename, O_RDONLY);
-   if (fd == -1) return true; // non-fatal
+  /* Try to get full command line */
+  snprintf (filename, MAX_NAME, "/proc/%d/cmdline", pid);
+  fd = open (filename, O_RDONLY);
+  if (fd == -1)
+    return true;                // non-fatal
 
-   size = xread(fd, buf, MAX_READ);
-   close(fd);
-   if (size <= 0) return true; // non-fatal
+  size = xread (fd, buf, MAX_READ);
+  close (fd);
+  if (size <= 0)
+    return true;                // non-fatal
 
-   for (int i = 0; i < size; i++)
-       if (buf[i] == '\0' || buf[i] == '\n')
-            buf[i] = ' ';
-   buf[size] = '\0';
-   free(process->comm);
-   process->comm = strdup(buf);
+  for (int i = 0; i < size; i++)
+    if (buf[i] == '\0' || buf[i] == '\n')
+      buf[i] = ' ';
+  buf[size] = '\0';
+  free (process->comm);
+  process->comm = strdup (buf);
 
-   return true;
+  return true;
 }
 
-long sysdep_uptime()
+long
+sysdep_uptime ()
 {
-   double uptime = 0;
-   FILE* fd = fopen("/proc/uptime", "r");
-   if (fd) {
-      fscanf(fd, "%64lf", &uptime);
-      fclose(fd);
-   }
-   return (long) uptime;
+  double uptime = 0;
+  FILE *fd = fopen ("/proc/uptime", "r");
+  if (fd)
+    {
+      fscanf (fd, "%64lf", &uptime);
+      fclose (fd);
+    }
+  return (long) uptime;
 }
--- sysdeps/solaris.c
+++ sysdeps/solaris.c
@@ -1,3 +1,5 @@
+/* Formatted with GNU indent -nut */
+
 #include <fcntl.h>
 #include <procfs.h>
 #include <stdio.h>
@@ -28,195 +30,227 @@ static unsigned kstat_initialized = 0;
  * no more.  If any kstat_read indicates that the chain has changed, then
  * the whole process is restarted.
  */
-int kstat_safe_namematch(int num, kstat_t **ksparg, char *name, void *buf, int size){
-   kstat_t *ks;
-   kstat_t **ksp;
-   kid_t new_kcid;
-   int namelen;
-   int count;
-   int changed;
-   char *cbuf;
+int
+kstat_safe_namematch (int num, kstat_t ** ksparg, char *name, void *buf,
+                      int size)
+{
+  kstat_t *ks;
+  kstat_t **ksp;
+  kid_t new_kcid;
+  int namelen;
+  int count;
+  int changed;
+  char *cbuf;
 
-   namelen = strlen(name);
+  namelen = strlen (name);
 
-   do {
+  do
+    {
       /* initialize before the scan */
-      cbuf = (char *)buf;
+      cbuf = (char *) buf;
       ksp = ksparg;
       count = 0;
       changed = 0;
 
       /* scan the chain for matching kstats */
-      for (ks = kc->kc_chain; ks != NULL; ks = ks->ks_next) {
-         if (strncmp(ks->ks_name, name, namelen) == 0) {
-            /* this kstat matches: save it if there is room */
-            if (count++ < num) {
-               /* read the kstat */
-               new_kcid = kstat_read(kc, ks, cbuf);
+      for (ks = kc->kc_chain; ks != NULL; ks = ks->ks_next)
+        {
+          if (strncmp (ks->ks_name, name, namelen) == 0)
+            {
+              /* this kstat matches: save it if there is room */
+              if (count++ < num)
+                {
+                  /* read the kstat */
+                  new_kcid = kstat_read (kc, ks, cbuf);
 
-               /* if the chain changed, update it */
-               if (new_kcid != kcid) {
-                  changed = 1;
-                  kcid = kstat_chain_update(kc);
+                  /* if the chain changed, update it */
+                  if (new_kcid != kcid)
+                    {
+                      changed = 1;
+                      kcid = kstat_chain_update (kc);
 
-                  /* there's no sense in continuing the scan */
-                  /* so break out of the for loop */
-                  break;
-               }
+                      /* there's no sense in continuing the scan */
+                      /* so break out of the for loop */
+                      break;
+                    }
 
-               /* move to the next buffers */
-               cbuf += size;
-               *ksp++ = ks;
+                  /* move to the next buffers */
+                  cbuf += size;
+                  *ksp++ = ks;
+                }
             }
-         }
-      }
-   } while(changed);
+        }
+    }
+  while (changed);
 
-   return count;
+  return count;
 }
 
 static kstat_t *ks_system_misc = NULL;
 
-int kstat_safe_retrieve(kstat_t **ksp, char *module, int instance, char *name, void *buf){
-   kstat_t *ks;
-   kid_t new_kcid;
-   int changed;
+int
+kstat_safe_retrieve (kstat_t ** ksp, char *module, int instance, char *name,
+                     void *buf)
+{
+  kstat_t *ks;
+  kid_t new_kcid;
+  int changed;
 
-   ks = *ksp;
-   do {
+  ks = *ksp;
+  do
+    {
       changed = 0;
       /* if we dont already have the kstat, retrieve it */
-      if (ks == NULL) {
-         if ((ks = kstat_lookup(kc, module, instance, name)) == NULL) {
-            return (-1);
-         }
-         *ksp = ks;
-      }
+      if (ks == NULL)
+        {
+          if ((ks = kstat_lookup (kc, module, instance, name)) == NULL)
+            {
+              return (-1);
+            }
+          *ksp = ks;
+        }
 
       /* attempt to read it */
-      new_kcid = kstat_read(kc, ks, buf);
+      new_kcid = kstat_read (kc, ks, buf);
       /* chance for an infinite loop here if kstat_read keeps 
          returning -1 */
 
       /* if the chain changed, update it */
-      if (new_kcid != kcid) {
-         changed = 1;
-         kcid = kstat_chain_update(kc);
-      }
-   } while (changed);
+      if (new_kcid != kcid)
+        {
+          changed = 1;
+          kcid = kstat_chain_update (kc);
+        }
+    }
+  while (changed);
 
-   return (0);
+  return (0);
 }
 
-int get_ncpus(){
-   kstat_named_t *kn;
-   int ret = -1;
+int
+get_ncpus ()
+{
+  kstat_named_t *kn;
+  int ret = -1;
 
-   if ((kn = kstat_data_lookup(ks_system_misc, "ncpus")) != NULL) {
-      ret = (int)(kn->value.ui32);
-   }
+  if ((kn = kstat_data_lookup (ks_system_misc, "ncpus")) != NULL)
+    {
+      ret = (int) (kn->value.ui32);
+    }
 
-   return ret;
+  return ret;
 }
 
 /* dummy */
-IOPriority sysdep_get_ioprio(pid_t p)
+IOPriority
+sysdep_get_ioprio (pid_t p)
 {
-    return 1;
+  return 1;
 }
 
 /* dummy */
-void sysdep_set_ioprio(pid_t p, IOPriority ioprio)
+void
+sysdep_set_ioprio (pid_t p, IOPriority ioprio)
 {
 }
 
-void sysdep_get_meminfo(ProcessList *this)
+void
+sysdep_get_meminfo (ProcessList * this)
 {
-    long pageSize;
+  long pageSize;
 
-    pageSize = sysconf(_SC_PAGESIZE) / 1024; // KiB
+  pageSize = sysconf (_SC_PAGESIZE) / 1024;     // KiB
 
-    this->totalMem = sysconf(_SC_PHYS_PAGES) * pageSize;
-    this->freeMem  = sysconf(_SC_AVPHYS_PAGES) * pageSize;
-    this->usedMem  = this->totalMem - this->freeMem;
+  this->totalMem = sysconf (_SC_PHYS_PAGES) * pageSize;
+  this->freeMem = sysconf (_SC_AVPHYS_PAGES) * pageSize;
+  this->usedMem = this->totalMem - this->freeMem;
 
-    struct anoninfo ai;
-    memset(&ai, 0, sizeof(ai));
-    (void) swapctl(SC_AINFO, &ai);
-    this->totalSwap = ai.ani_max * pageSize;
-    this->usedSwap  = (ai.ani_max - ai.ani_free) * pageSize;
+  struct anoninfo ai;
+  memset (&ai, 0, sizeof (ai));
+  (void) swapctl (SC_AINFO, &ai);
+  this->totalSwap = ai.ani_max * pageSize;
+  this->usedSwap = (ai.ani_max - ai.ani_free) * pageSize;
 
-    /*TODO*/
-    this->sharedMem = 0;
-    this->cachedMem = 0;
-    this->buffersMem = 0;
+   /*TODO*/ this->sharedMem = 0;
+  this->cachedMem = 0;
+  this->buffersMem = 0;
 }
 
 /* TODO */
-void sysdep_update_cpu_data(ProcessList *this)
+void
+sysdep_update_cpu_data (ProcessList * this)
 {
-   static kstat_t **cpu_ks = NULL;
-   static cpu_stat_t *cpu_stat = NULL;
-   static unsigned int nelems = 0;
-   cpu_stat_t *cpu_stat_p;
-   int i, cpu_num;
+  static kstat_t **cpu_ks = NULL;
+  static cpu_stat_t *cpu_stat = NULL;
+  static unsigned int nelems = 0;
+  cpu_stat_t *cpu_stat_p;
+  int i, cpu_num;
 
-   if(!kstat_initialized) {
+  if (!kstat_initialized)
+    {
       /* open kstat */
-      if ((kc = kstat_open()) == NULL) {
-         fprintf(stderr, "Unable to open kstat.\n");
-         return;
-      }
+      if ((kc = kstat_open ()) == NULL)
+        {
+          fprintf (stderr, "Unable to open kstat.\n");
+          return;
+        }
       kcid = kc->kc_chain_id;
-      kstat_safe_retrieve(&ks_system_misc, "unix", 0, "system_misc", NULL);
+      kstat_safe_retrieve (&ks_system_misc, "unix", 0, "system_misc", NULL);
       kstat_initialized = 1;
-   }
+    }
 
-   while (nelems > 0 ?
-         (cpu_num = kstat_safe_namematch(nelems,
-                                         cpu_ks,
-                                         "cpu_stat",
-                                         cpu_stat,
-                                         sizeof(cpu_stat_t))) > nelems :
-         (cpu_num = get_ncpus()) > 0)
-   {
+  while (nelems > 0 ?
+         (cpu_num = kstat_safe_namematch (nelems,
+                                          cpu_ks,
+                                          "cpu_stat",
+                                          cpu_stat,
+                                          sizeof (cpu_stat_t))) > nelems :
+         (cpu_num = get_ncpus ()) > 0)
+    {
       /* reallocate the arrays */
       nelems = cpu_num;
-      if (cpu_ks != NULL) {
-         free(cpu_ks);
-      }
-      cpu_ks = (kstat_t **)calloc(nelems, sizeof(kstat_t *));
-      if (cpu_stat != NULL) {
-         free(cpu_stat);
-      }
-      cpu_stat = (cpu_stat_t *)malloc(nelems * sizeof(cpu_stat_t));
-   }
-   cpu_stat_p = cpu_stat;
+      if (cpu_ks != NULL)
+        {
+          free (cpu_ks);
+        }
+      cpu_ks = (kstat_t **) calloc (nelems, sizeof (kstat_t *));
+      if (cpu_stat != NULL)
+        {
+          free (cpu_stat);
+        }
+      cpu_stat = (cpu_stat_t *) malloc (nelems * sizeof (cpu_stat_t));
+    }
+  cpu_stat_p = cpu_stat;
 
-   unsigned long long int
-       usertime = 0, nicetime = 0, systemtime = 0,
-       systemalltime = 0, idlealltime = 0, idletime = 0,
-       totaltime = 0, virtalltime = 0;
+  unsigned long long int
+    usertime = 0, nicetime = 0, systemtime = 0,
+    systemalltime = 0, idlealltime = 0, idletime = 0,
+    totaltime = 0, virtalltime = 0;
 
-   for (int i = 0; i <= this->cpuCount; i++) {
+  for (int i = 0; i <= this->cpuCount; i++)
+    {
       unsigned long long int ioWait, irq, softIrq, steal, guest;
       ioWait = irq = softIrq = steal = guest = 0;
 
-      if(i==0){
-         // This is the "average" bar I guess. skip for now.
-         continue;
-      }
+      if (i == 0)
+        {
+          // This is the "average" bar I guess. skip for now.
+          continue;
+        }
 
       usertime = cpu_stat_p->cpu_sysinfo.cpu[CPU_USER];
       systemtime = cpu_stat_p->cpu_sysinfo.cpu[CPU_KERNEL];
       idletime = cpu_stat_p->cpu_sysinfo.cpu[CPU_IDLE];
-      ioWait = cpu_stat_p->cpu_sysinfo.wait[W_IO] + cpu_stat_p->cpu_sysinfo.wait[W_PIO];
+      ioWait =
+        cpu_stat_p->cpu_sysinfo.wait[W_IO] +
+        cpu_stat_p->cpu_sysinfo.wait[W_PIO];
 
       idlealltime = idletime + ioWait;
       systemalltime = systemtime + irq + softIrq;
       virtalltime = steal + guest;
-      totaltime = usertime + nicetime + systemalltime + idlealltime + virtalltime;
-      CPUData* cpuData = &(this->cpus[i]);
+      totaltime =
+        usertime + nicetime + systemalltime + idlealltime + virtalltime;
+      CPUData *cpuData = &(this->cpus[i]);
       cpuData->userPeriod = usertime - cpuData->userTime;
       cpuData->nicePeriod = nicetime - cpuData->niceTime;
       cpuData->systemPeriod = systemtime - cpuData->systemTime;
@@ -243,90 +277,101 @@ void sysdep_update_cpu_data(ProcessList *this)
       cpuData->totalTime = totaltime;
 
       cpu_stat_p++;
-   }
+    }
 }
 
-int sysdep_max_pid()
+int
+sysdep_max_pid ()
 {
-    return sysconf(_SC_MAXPID);
+  return sysconf (_SC_MAXPID);
 }
 
-bool sysdep_get_process_info(Process *process, pid_t pid)
+bool
+sysdep_get_process_info (Process * process, pid_t pid)
 {
-    char filename[34];
-    int fd;
-    ssize_t r;
-    psinfo_t info;
-    prusage_t usage;
+  char filename[34];
+  int fd;
+  ssize_t r;
+  psinfo_t info;
+  prusage_t usage;
 
-    snprintf(filename, sizeof(filename), "/proc/%d/psinfo", pid);
-    fd = open(filename, O_RDONLY);
-    if (-1 == fd) return false;
-    r = xread(fd, &info, sizeof(info));
-    (void) close(fd);
-    if (!r) return false;
+  snprintf (filename, sizeof (filename), "/proc/%d/psinfo", pid);
+  fd = open (filename, O_RDONLY);
+  if (-1 == fd)
+    return false;
+  r = xread (fd, &info, sizeof (info));
+  (void) close (fd);
+  if (!r)
+    return false;
 
-    snprintf(filename, sizeof(filename), "/proc/%d/usage", pid);
-    fd = open(filename, O_RDONLY);
-    if (-1 == fd) return false;
-    r = xread(fd, &usage, sizeof(usage));
-    (void) close(fd);
-    if (!r) return false;
+  snprintf (filename, sizeof (filename), "/proc/%d/usage", pid);
+  fd = open (filename, O_RDONLY);
+  if (-1 == fd)
+    return false;
+  r = xread (fd, &usage, sizeof (usage));
+  (void) close (fd);
+  if (!r)
+    return false;
 
-    process->state = info.pr_lwp.pr_sname;
+  process->state = info.pr_lwp.pr_sname;
 
-    process->st_uid = info.pr_uid;
+  process->st_uid = info.pr_uid;
 
-    process->ppid = info.pr_ppid;
-    process->pgrp = info.pr_pgid;
-    process->session = info.pr_sid;
-    process->tty_nr = info.pr_ttydev;
-    process->tpgid = -1; // FIXME
-    process->flags = info.pr_flag;
+  process->ppid = info.pr_ppid;
+  process->pgrp = info.pr_pgid;
+  process->session = info.pr_sid;
+  process->tty_nr = info.pr_ttydev;
+  process->tpgid = -1;          // FIXME
+  process->flags = info.pr_flag;
 
-    long tix = sysconf(_SC_CLK_TCK);
-    process->utime = usage.pr_utime.tv_sec * tix;
-    process->stime = usage.pr_stime.tv_sec * tix;
+  long tix = sysconf (_SC_CLK_TCK);
+  process->utime = usage.pr_utime.tv_sec * tix;
+  process->stime = usage.pr_stime.tv_sec * tix;
 
-    process->priority = info.pr_lwp.pr_pri;
-    process->nice = info.pr_lwp.pr_nice - 20;
+  process->priority = info.pr_lwp.pr_pri;
+  process->nice = info.pr_lwp.pr_nice - 20;
 
-    process->nlwp = info.pr_nlwp;
+  process->nlwp = info.pr_nlwp;
 
-    // FIXME:
-    process->exit_signal = SIGCHLD;
+  // FIXME:
+  process->exit_signal = SIGCHLD;
 
-    process->processor = info.pr_lwp.pr_onpro;
+  process->processor = info.pr_lwp.pr_onpro;
 
-    process->comm = strdup(info.pr_psargs);
+  process->comm = strdup (info.pr_psargs);
 
 
-    /* XXX root only: */
-    snprintf(filename, sizeof(filename), "/proc/%d/status", pid);
-    fd = open(filename, O_RDONLY);
-    if (fd > 0) {
-        pstatus_t status;
-        if (xread(fd, &status, sizeof(status)) > 0) {
-            process->cutime = status.pr_cutime.tv_sec * tix;
-            process->cstime = status.pr_cstime.tv_sec * tix;
+  /* XXX root only: */
+  snprintf (filename, sizeof (filename), "/proc/%d/status", pid);
+  fd = open (filename, O_RDONLY);
+  if (fd > 0)
+    {
+      pstatus_t status;
+      if (xread (fd, &status, sizeof (status)) > 0)
+        {
+          process->cutime = status.pr_cutime.tv_sec * tix;
+          process->cstime = status.pr_cstime.tv_sec * tix;
         }
-        (void) close(fd);
+      (void) close (fd);
     }
 
 
-    return true;
+  return true;
 }
 
-long sysdep_uptime()
+long
+sysdep_uptime ()
 {
-    long uptime = 0;
-    int fd;
-    if ((fd = open("/proc/0/psinfo", O_RDONLY)) > 0) {
-        psinfo_t pid0;
-        if (xread(fd, &pid0, sizeof(pid0)) > 0) {
-            uptime = time(NULL) - pid0.pr_start.tv_sec;
+  long uptime = 0;
+  int fd;
+  if ((fd = open ("/proc/0/psinfo", O_RDONLY)) > 0)
+    {
+      psinfo_t pid0;
+      if (xread (fd, &pid0, sizeof (pid0)) > 0)
+        {
+          uptime = time (NULL) - pid0.pr_start.tv_sec;
         }
-        (void) close(fd);
+      (void) close (fd);
     }
-    return uptime;
+  return uptime;
 }
